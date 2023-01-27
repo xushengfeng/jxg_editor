e26e1c9ca4351a31a4243c892d2229f2e9d5494c
xushengfeng <xushengfeng_zg@163.com>
2023-01-28 01:50:37 +0800

    主要 输入代码编辑

--- src/js/js.ts
+++ src/js/js.ts
@@ -1,1 +1,28 @@
 import JXG from "jsxgraph";
+import * as monaco from "monaco-editor";
+
+const code = document.getElementById("code");
+
+if (JXG) {
+}
+
+let editor = monaco.editor.create(code, {
+    value: `let brd=JXG.JSXGraph.initBoard(gid,{axis:true,showCopyRight:false,boundingbox:[-4,4,4,-4]});`,
+    language: "javascript",
+    minimap: { enabled: false },
+    automaticLayout: true,
+    codeLens: true,
+    colorDecorators: true,
+    contextmenu: false,
+    readOnly: false,
+    formatOnPaste: true,
+    overviewRulerBorder: false,
+    scrollBeyondLastLine: true,
+    fontSize: 12,
+    wordWrap: "on",
+});
+
+editor.onDidChangeModelContent((e) => {
+    console.log(editor.getValue());
+    eval(`{let gid = 'board';${editor.getValue()}}`);
+});
