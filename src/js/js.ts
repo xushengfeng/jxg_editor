9197788f25d2ff4a493a5bbfb7e21d774ddec7fa
xushengfeng <xushengfeng_zg@163.com>
2023-01-28 16:14:46 +0800

    编辑 插入数学公式

--- src/js/js.ts
+++ src/js/js.ts
@@ -3,8 +3,13 @@ import loader from "@monaco-editor/loader";
 loader.config({ paths: { vs: "https://unpkg.com/monaco-editor@0.34.1/min/vs" } }); // or local
 let monaco = await loader.init();
 import jxg_type from "../../lib/index.d.ts?raw";
+import "//unpkg.com/mathlive";
+import { ComputeEngine } from "@cortex-js/compute-engine";
 
 const code = document.getElementById("code");
+const mfe = document.querySelector("math-field");
+const add_math = document.getElementById("add_math");
+const add_function = document.getElementById("add_function");
 
 if (JXG) {
 }
@@ -37,3 +42,30 @@ editor.onDidBlurEditorText(() => {
 function run_code(code: string) {
     eval(`{let gid = 'board';${code}}`);
 }
+
+function get_math_value() {
+    // @ts-ignore
+    return mfe.value as string;
+}
+
+add_math.onclick = () => {
+    editor.trigger("keyboard", "type", { text: String(run_math(get_math_value())) });
+    editor.focus();
+};
+add_function.onclick = () => {
+    let code = `(x)=>run_math("${get_math_value().replaceAll("\\", "\\\\")}", {x})`;
+    editor.trigger("keyboard", "type", { text: code });
+    editor.focus();
+};
+
+const ce = new ComputeEngine();
+let parse_o: { [key: string]: ReturnType<typeof ce.parse> } = {};
+let run_math = (window["run_math"] = (code: string, v?: { [key: string]: number }) => {
+    if (!v) v = {};
+    let ex = parse_o[code];
+    if (!ex) {
+        ex = parse_o[code] = ce.parse(code);
+    }
+    let out = ex.subs(v).N().valueOf();
+    return out;
+});
